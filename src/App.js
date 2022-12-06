a667eeeac4780d05845e495b128b163cda04b04c
charankanumuri <charan.kanumuri@gmail.com>
2022-12-06 12:57:47 -0800

    configure amplify auth changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { fetchData } from "../src/api";
 import coronaImage from "./images/coronaImage.png";
 import { DataStore } from "@aws-amplify/datastore";
 import { CovidReport } from "./models";
-// import Authenticator from "./components/Authenticator";
+import Authenticator from "./components/Authenticator";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 
@@ -25,7 +25,7 @@ class App extends Component {
   }
 
   dataseed = async (Recovered, Infected, Died, Country, lastUpdatedAt) => {
-    console.log(Country);
+    // console.log(Country);
     const data = await DataStore.save(
       new CovidReport({
         Recovered,
@@ -60,7 +60,7 @@ class App extends Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
-        {/* <Authenticator /> */}
+        <Authenticator />
         <img className={styles.image} src={coronaImage} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,34 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://xdmlgb46azh5jfgehdtwyzypde.appsync-api.us-west-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-west-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-w2qpwjiglvawzkfnz27yfhuxum"
+    "aws_appsync_apiKey": "da2-w2qpwjiglvawzkfnz27yfhuxum",
+    "aws_cognito_identity_pool_id": "us-west-1:b8c045d0-30ed-4a1d-b2b1-24f3dd64fb7e",
+    "aws_cognito_region": "us-west-1",
+    "aws_user_pools_id": "us-west-1_kPw47CmRK",
+    "aws_user_pools_web_client_id": "2a7gbt9ebscb513ugh3fa5sf7l",
+    "oauth": {},
+    "aws_cognito_username_attributes": [],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "PREFERRED_USERNAME",
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": [
+            "REQUIRES_LOWERCASE",
+            "REQUIRES_NUMBERS",
+            "REQUIRES_SYMBOLS",
+            "REQUIRES_UPPERCASE"
+        ]
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
 };
 
 

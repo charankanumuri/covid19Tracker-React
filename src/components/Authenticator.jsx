d3b9e10f7ffef1456bd3512e2f46ac4ddd8c963e
charankanumuri <charan.kanumuri@gmail.com>
2022-12-07 01:23:52 -0800

    AppBar modified

--- src/components/Authenticator.jsx
+++ src/components/Authenticator.jsx
@@ -4,16 +4,21 @@ import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 import awsExports from "../aws-exports.js";
-import { AppBar, Toolbar, Box } from "@material-ui/core";
+import { AppBar, Toolbar, Box, Tab } from "@material-ui/core";
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   return (
-    <AppBar position="static" width="100%">
+    <AppBar position="relative" width="100%">
       <Toolbar>
-        <Box display="flex" flexGrow={1}>
-          <h2>Welcome {user.username}</h2>
-          <h3>Wear a Mask to save the risk!</h3>
+        <Box display="flex" flexGrow={1} alignItems="center">
+          <h4>Welcome {user.username}!</h4>
+          <h4>
+            <Tab />
+            <Tab />
+            <Tab />
+            Wear a Mask to save the risk.
+          </h4>
         </Box>
         <button alignItems="right" onClick={signOut}>
           Sign out

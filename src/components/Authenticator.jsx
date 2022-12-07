4c4d038abf9c859b18a0f6f59c3df3f1a6fd096c
charankanumuri <charan.kanumuri@gmail.com>
2022-12-06 21:15:33 -0800

    AppBar included

--- src/components/Authenticator.jsx
+++ src/components/Authenticator.jsx
@@ -4,14 +4,21 @@ import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 import awsExports from "../aws-exports.js";
+import { AppBar, Toolbar, Box } from "@material-ui/core";
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
+    <AppBar position="static" width="100%">
+      <Toolbar>
+        <Box display="flex" flexGrow={1}>
+          <h2>Welcome {user.username}</h2>
+        </Box>
+        <button alignItems="right" onClick={signOut}>
+          Sign out
+        </button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
